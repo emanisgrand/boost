15f6bd4ae1c6a1f8e4314d3b622fe75d3eed65de
emanisgrand <tenhitcombo@gmail.com>
2024-02-14 10:52:46 -0500

    SPRINT 5

--- arm_1.gd
+++ arm_1.gd
@@ -1,16 +1,13 @@
 extends Node3D
 @onready var animation_player = $AnimationPlayer
-@onready var physical_bone_00 = $"arm_skeleton/Skeleton3D/Physical Bone Bone_004"
 @onready var skeleton_3d = $arm_skeleton/Skeleton3D
-@onready var physical_bone = $"arm_skeleton/Skeleton3D/Physical Bone Bone_006"
 
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	animation_player.play("armLoop")
-	physical_bone.get_simulate_physics()
+	#skeleton_3d.physical_bones_start_simulation()
 	pass # Replace with function body.
-
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
