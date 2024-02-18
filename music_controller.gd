8868ed6d8ee362480e5012363d21c2ee0c174b34
emanisgrand <tenhitcombo@gmail.com>
2024-02-18 14:45:14 -0500

    adding music sync

--- music_controller.gd
+++ music_controller.gd
@@ -1,10 +1,13 @@
 extends Node
 
-
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	for child:AudioStreamPlayer in get_children():
+		child.play()
 
+func fade_in():
+	# Fade in horns
+	pass
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
