63c14ccebfc8355043695b5fc4343255e040f5cd
Eman Martinez <emanisgrand@users.noreply.github.com>
2024-03-13 07:32:48 -0400

    Delete Builds directory

--- Builds/WEB/index/Levels/LandingPad.gd
+++
@@ -1,2 +0,0 @@
-extends CSGBox3D
-@export_file("*.tscn") var file_path 
--- Builds/WEB/index/Levels/TitleScreen.gd
+++
@@ -1,9 +0,0 @@
-extends Node3D
-
-var level_scene = preload("res://Levels/Level.tscn")
-
-func _on_start_button_pressed() -> void:
-	get_tree().change_scene_to_packed(level_scene)
-
-func _on_quit_button_pressed() -> void:
-	get_tree().quit()
